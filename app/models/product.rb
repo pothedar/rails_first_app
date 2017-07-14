5c71d58cb49d8c0d752212058f72a0481936326f
Srikanth Pothedar <spothedar@guardanthealth.com>
2017-07-14 11:12:12 -0700

    4.6 add form validation and remove html href in ERBs
    added carrierwave gem file to support image uploads

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ApplicationRecord
+  mount_uploader :image_url, ImageUploader
+  validates_processing_of :image_url
+  validate :image_size_validation
+  validates :name, :description, :colour, :price, :presence => true
+  private
+    def image_size_validation
+      errors[:image_url] << "should be larger than 50KB" if image_url.size < 0.05.megabytes
+    end
 end
