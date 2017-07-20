2472ff7fde992ce9e88453772aa320cf84a4bccc
Srikanth Pothedar <srikanth.pothedar@varian.com>
2017-07-19 17:59:41 -0700

    5.2 add search form and styles

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,8 @@ class Product < ApplicationRecord
     def image_size_validation
       errors[:image_url] << "should be larger than 50KB" if image_url.size < 0.05.megabytes
     end
+    def self.search(search_term)
+		like_string = Rails.env.production? ? "ilike" : "LIKE"
+		Product.where("name #{like_string} ?", "%#{search_term}%")
+    end
 end
